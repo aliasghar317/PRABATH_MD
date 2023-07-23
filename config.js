3fef4a00e02df95977724512ee2e8deac48b9ef4
aliasghar317 <135347876+aliasghar317@users.noreply.github.com>
2023-07-23 10:16:26 +0500

    Update config.js

--- config.js
+++ config.js
@@ -12,16 +12,16 @@ global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
 global.devs = '94762280384';
 global.website = 'https://github.com/prabathLK/PRABATH-MD' //wa.me/+94762280384
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/H062oEI.jpeg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/YaO9Hrx.jpeg'
 module.exports = {
   OPENAI_API_KEY:process.env.OPENAI_API_KEY || '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',
-  botname: process.env.BOT_NAME || 'PRABATH-MD',
+  botname: process.env.BOT_NAME || '𝚫𝐒𝐆𝚮𝚫𝚪-𝐉𝚫𝚻𝚯𝚰',
   dl_limit: process.env.DL_SIZE || '300',
   art: process.env.AUTO_RESTART || 'true',
   sudo: process.env.SUDO || '94762280384',
-  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/ad25b2227fa2a1a01b707.jpg',
-  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY PRABATH-MD* 🎊',
-  ownername:process.env.OWNER_NAME || 'PRABATH',
+  alivepic: process.env.ALIVE_IMG || 'https://telegra.ph/file/7524772002c61b06b4861.jpg',
+  alivemsj: process.env.ALIVE_MSJ || '*POWERD BY 𝚫𝐒𝐆𝚮𝚫𝚪 𝐉𝚫𝚻𝚯𝚰* 🎊',
+  ownername:process.env.OWNER_NAME || '𝚫𝐒𝐆𝚮𝚫𝚪 𝐉𝚫𝚻𝚯𝚰',
   sessionName: process.env.SESSION_ID || 'PUT-HERE',
   author: process.env.PACK_INFO.split(";")[0] || 'author', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'false',
@@ -43,7 +43,7 @@ module.exports = {
   ALIVE_MESSAGE: process.env.ALIVE_MESSAGE || '',
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || 'put-key-here',
   VERSION: process.env.VERSION === undefined ? 'v.5.0.0' : process.env.VERSION,
-  LANG: process.env.THEME|| 'PRABATH-MD',
+  LANG: process.env.THEME|| '𝚫𝐒𝐆𝚮𝚫𝚪-𝐉𝚫𝚻𝚯𝚰',
   WORKTYPE: process.env.WORK_TYPE === undefined ? 'public' : process.env.WORK_TYPE
 };
 
